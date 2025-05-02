295a75014a1131c517835d933a9b82fd06eb5758
Matthew Catalfamo <89117055+MatthewSword7272@users.noreply.github.com>
2025-05-02 12:47:52 +1000

    Seperated the login and signup pages

--- pages/signup.vue
+++ pages/signup.vue
@@ -3,17 +3,21 @@
         <form
             class="absolute py-10 px-8 flex flex-col justify-around inset-1/2 -translate-1/2 max-h-[600px] min-h-[500px] md:w-[500px] w-full rounded bg-red-500 shadow-2xl">
             <div class="flex flex-col gap-2">
+                <label for="name">Name</label>
+                <input required name="name" v-model="signupData.name" type="text" />
+            </div>
+            <div class="flex flex-col gap-2">
                 <label for="username">Username</label>
-                <input required name="username" v-model="loginData.username" type="text" />
+                <input required name="username" v-model="signupData.username" type="text" />
             </div>
             <div class="flex flex-col gap-2">
                 <label for="password">Password</label>
-                <input required name="password" type="password" v-model="loginData.password" />
+                <input required name="password" type="password" v-model="signupData.password" />
             </div>
             <div class="text-center">
-                <span>Don't have an account? <NuxtLink to="/signup" id="link">Sign Up</NuxtLink></span>
+                <span>Already have an account? <NuxtLink to="/login" id="link">Login</NuxtLink></span>
             </div>
-            <button id="mainButton" @click.prevent="login">Login</button>
+            <button id="mainButton" @click.prevent="registerUser">Sign Up</button>
         </form>
     </div>
 </template>
@@ -27,44 +31,40 @@ definePageMeta({
 
 const config = useRuntimeConfig();
 
-const loginData = ref({
+const signupData = ref({
+    name: '',
     username: '',
     password: '',
 });
 
 const { storeUser } = useUsers();
 
-const login = async () => {
+const registerUser = async () => {
     try {
-        const { data, error } = await useFetch(`${config.app.apiURL}/users/verify`, {
+        const { data } = await useFetch(`${config.app.apiURL}/users`, {
             method: 'POST',
-            body: {
-                username: loginData.value.username,
-                password: loginData.value.password
-            }
+            body: signupData.value,
         });
-
-        if (error.value) {
-            alert('Invalid credentials. Please Try Again');
-            return;
-        }
 
         if (data.value) {
             storeUser(data.value);
-            alert('Login successful!');
+            alert('Account created successfully!');
             navigateTo('/');
+        } else {
+            alert('Account created but no user data returned');
+            navigateTo('/login');
         }
     } catch (error) {
-        console.error('Login error:', error);
-        alert('An error occurred during login. Please try again');
+        console.error('Registration error:', error);
+        alert('Error creating account. Please try again.');
     }
 
-    loginData.value = {
+    signupData.value = {
+        name: '',
         username: '',
         password: '',
     };
 }
-
 </script>
 
 <style scoped>
